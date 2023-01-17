c170f689fc6df37e5f0e978e40099415ed2594d9
Rutuja412 <rutujakadam4545@gmail.com>
2023-01-17 18:21:40 +0530

    [Rutuja]:Adding Program for Maximum string

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -7,9 +7,10 @@ namespace Generics
         static void Main(string[] args)
         {
             Console.WriteLine("-----Generics------");
-            Console.WriteLine("Maximun Float number  is :");
+            Console.WriteLine("Maximun string   is :");
             // Console.WriteLine(MaximumNumberCheck.MaximumIntegerNumber(100, 200, 300));
-            Console.WriteLine(MaximumNumberCheck.MaximumFloatNumber(1.11, 2.22, 4.44));
+            //Console.WriteLine(MaximumNumberCheck.MaximumFloatNumber(1.11, 2.22, 4.44));
+            Console.WriteLine(MaximumNumberCheck.MaximumStringNumber("Apple", "Peach", "Banana"));
         }
     }
 }
