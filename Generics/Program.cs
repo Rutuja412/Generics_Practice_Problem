a3c2eea807b7c78cf98efa44164c6491ca2e8416
Rutuja412 <rutujakadam4545@gmail.com>
2023-01-17 15:00:20 +0530

    [Rutuja]:Adding Program for Maximum Integer

---
+++ Generics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----Generics------");
+            Console.WriteLine("Maximun Integer is :");
+            Console.WriteLine(MaximumNumberCheck.MaximumIntegerNumber(100, 200, 300));
+        }
+    }
+}
